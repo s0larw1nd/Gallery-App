25906aa1241d6064eb7556d5d53ca369f38d8977
s0larw1nd <alixess13@gmail.com>
2025-09-21 18:59:13 +0300

    lab3

--- app/src/main/java/pack/gallery/MainActivity.kt
+++ app/src/main/java/pack/gallery/MainActivity.kt
@@ -1,12 +1,17 @@
 package pack.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,20 +21,46 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import pack.gallery.ui.theme.GalleryTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var btn: Button
     private lateinit var img: ImageView
 
+    @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn = findViewById(R.id.button)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         img = findViewById(R.id.imageView)
 
         val descripts = listOf("Picture 1")
 
         //btn.setOnClickListener { Toast.makeText(this, "Привет, мир!", Toast.LENGTH_SHORT).show() }
         img.setOnClickListener { Toast.makeText(this, descripts[0], Toast.LENGTH_SHORT).show() }
+    }
+
+    @Override
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    @Override
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Обработка нажатия "Настройки"
+                true
+            }
+            R.id.action_about -> {
+                // Обработка нажатия "О приложении"
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
